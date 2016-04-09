e757c4e4580f49af2f90b01d5e76c653b3c1418d
Lucas de Souza <lde.souza@yahoo.com>
2016-04-09 19:44:55 -0300

    Utilizando métodos de pesquisa e filtro da API de Stream

--- src/br/com/softplan/controller/PedidoController.java
+++ src/br/com/softplan/controller/PedidoController.java
@@ -2,6 +2,7 @@ package br.com.softplan.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import br.com.softplan.model.dao.PedidoDAO;
 import br.com.softplan.model.domain.Categoria;
@@ -12,59 +13,33 @@ public class PedidoController {
 
 	public void salvarPedidoDeLimpeza(Pedido pedido) {
 		List<Item> itens = pedido.getItens();
-		List<Item> itensDeLimpeza = new ArrayList<>();
-		for (Item item : itens) {
-			if (item.isProdutoDeLimpeza()){
-				itensDeLimpeza.add(item);
-			}
-		}
-		
+		List<Item> itensDeLimpeza = itens.stream().filter(item -> item.isProdutoDeLimpeza()).collect(Collectors.toList());
 		pedido.setItens(itensDeLimpeza);
 		new PedidoDAO().salvar(pedido);
 	}
 	
 	public boolean todosItensSaoDe(List<Item> itens, Categoria categoria){
-		for (Item item : itens) {
-			if (!item.getCategoria().equals(categoria)){
-				return false;
-			}
-		}
-		return true;
+		return itens.stream().allMatch(item -> item.getCategoria().equals(categoria));
 	}
 	
 	public boolean nenhumItemEstaSemValor(List<Item> itens){
-		for (Item item : itens) {
-			if (item.getVlUnitario() == null || item.getVlUnitario() == 0){
-				return false;
-			}
-		}
-		return true;
+		return itens.stream().noneMatch(item -> item.getVlUnitario() == null || item.getVlUnitario() == 0);
 	}
 	
 	public boolean existeItemSuperiorAoValorDeReferencia(List<Item> itens , Double valorReferencia){
-		for (Item item : itens) {
-			if (item.getVlUnitario() > valorReferencia){
-				return true;
-			}
-		}
-		
-		return false;
+		return itens.stream().anyMatch(item -> item.getVlUnitario() > valorReferencia);
 	}
 	
-	public void aplicarDescontoEmAlgumItemDeLimpeza(List<Item> itens, Double desconto){
-		for (Item item : itens) {
-			if (item.isProdutoDeLimpeza()){
-				item.aplicarDesconto(desconto);
-				break;
-			}
-		}
-	}
 	
 	public Item buscarItemMaisBarato(List<Item> itens){
 		itens.sort((item1,item2) -> item1.getVlUnitario().compareTo(item2.getVlUnitario()));
-		return itens.get(0);
+		return itens.stream().findFirst().get();
 	}
 	
+	public void aplicarDescontoEmAlgumItemDeLimpeza(List<Item> itens, Double desconto){
+		Item item = itens.stream().filter(item2 -> item2.isProdutoDeLimpeza()).findAny().get();
+		item.aplicarDesconto(desconto);
+	}
 	
 	public static void main(String[] args) {
 		PedidoController pedidoController = new PedidoController();
